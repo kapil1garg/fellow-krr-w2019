4a139f73f806b692fa7c63d1f38f07bddfb1e63b
Kapil Garg <kapil1garg@gmail.com>
2019-03-05 02:38:54 -0600

    finished scrapping

--- scraping/Person.py
+++ scraping/Person.py
@@ -1,3 +1,179 @@
 class Person(object):
-    def __init__(self):
+    def __init__(self, first_name, last_name, year, major, academic_interests, post_grad_goal, software_experience,
+                 hobbies):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.year = self.remap_year(year)
+        self.major = self.remap_major(major)
+        self.academic_interests = self.remap_academic_interests(academic_interests.split(', '))
+        self.post_grad_goal = self.remap_post_grad_goal(post_grad_goal)
+        self.software_experience = self.remap_software_experience(software_experience.split(', '))
+        self.hobbies = self.remap_hobbies(hobbies.split(', '))
+
+    """
+    Data Remapping Functions
+    """
+    def remap_year(self, orig_value):
+        """
+        Remaps values from survey education year to year for KB.
+
+        :param orig_value: (string) value to be remapped.
+        :return: (string) remapped education year.
+        """
+        remapping = {
+            'Freshman': 'Undergrad',
+            'Sophomore': 'Undergrad',
+            'Junior': 'Undergrad',
+            'Senior': 'Undergrad',
+            'Masters Student': 'Masters',
+            'PhD Student': 'PhD',
+            'Post-Doc': 'PostDoc'
+        }
+
+        return self.remap_single_value(orig_value, remapping)
+
+    def remap_major(self, orig_value):
+        """
+        Remaps values from survey major to field of study for KB.
+
+        :param orig_value: (string) value to be remapped.
+        :return: (string) remapped major.
+        """
+        remapping = {
+            'Computer Science': 'CompSci',
+            'Cognitive Science': 'CogSci',
+            'Masters in AI (MSAI)': 'AI',
+            'Masters in Robotics (MSR)': 'Robotics',
+            'Masters in Analytics (MSiA)': 'DataScience',
+            'Other': 'Other'
+        }
+
+        return self.remap_single_value(orig_value, remapping)
+
+    def remap_academic_interests(self, orig_values):
+        """
+        Remaps values from survey academic interests to interests for KB.
+
+        :param orig_values: (list of strings) list of values to be remapped.
+        :return: (list of strings) remapped academic interests
+        """
+        remapping = {
+            'Artificial Intelligence (AI)': 'ArtificialIntelligence',
+            'Machine Learning (ML)': 'MachineLearning',
+            'Human-Computer Interaction (HCI)': 'HumanComputerInteraction',
+            'Computer Vision': 'ComputerVision',
+            'Game Development': 'GameDevelopment',
+            'Graphics': 'Graphics',
+            'Robotics': 'Robotics',
+            'Networking': 'Networking',
+            'Systems': 'ComputerScienceSystems',
+            'Computational Theory': 'ComputerScienceTheory'
+        }
+
+        return self.remap_list_values(orig_values, remapping)
+
+    def remap_post_grad_goal(self, orig_value):
+        """
+        Remaps values from survey post-grad goal to goals for KB.
+
+        :param orig_value: (string) value to be remapped.
+        :return: (string) remapped post-grad goal.
+        """
+        remapping = {
+            'Software Engineer': 'SoftwareEngineer',
+            'Project Manager': 'ProjectManager',
+            'Consultant': 'Consultant',
+            'Post-Doc': 'PostDoc',
+            'Faculty': 'Faculty',
+            'Industry Research': 'IndustryResearch',
+            'Other': 'Other'
+        }
+
+        return self.remap_single_value(orig_value, remapping)
+
+    def remap_software_experience(self, orig_values):
+        """
+        Remaps values from survey software experience to experience for KB.
+
+        :param orig_values: (list of strings) list of values to be remapped.
+        :return: (list of strings) remapped software experience.
+        """
+        remapping = {
+            'Backend web development': 'BackendWeb',
+            'Frontend web development': 'FrontendWeb',
+            'Game Development': 'GameDev',
+            'iOS mobile development': 'iOSDev',
+            'Android development': 'AndroidDev',
+            'Data Science and Machine Learning': 'DataScience',
+            'Hardware (e.g. Arduinos; Raspberry Pi; etc.)': 'Hardware'
+        }
+
+        return self.remap_list_values(orig_values, remapping)
+
+    def remap_hobbies(self, orig_values):
+        """
+        Remaps values from survey hobbies to hobbies for KB.
+
+        :param orig_values: (list of strings) list of values to be remapped.
+        :return: (list of strings) remapped hobbies.
+        """
+        remapping = {
+            'Sports': 'Sports',
+            'Movies': 'Movies',
+            'Traveling': 'Traveling',
+            'Art and Culture': 'Art',
+            'Music': 'Music',
+            'Food': 'Food',
+            'Hackathons': 'Hackathons'
+        }
+
+        return self.remap_list_values(orig_values, remapping)
+
+    """
+    Utility Functions for Remapping
+    """
+    @staticmethod
+    def remap_list_values(orig_values, remapping):
+        """
+        Remaps values in a list to new values based on remapping dict.
+
+        :param orig_values: (list of strings) list of values to be remapped.
+        :param remapping: (dict of strings) remapping from orig_values to new values.
+        :return: (list of strings) list of remapped values based on remappings.
+        """
+        return [remapping[x] if x in remapping else x for x in orig_values]
+
+    @staticmethod
+    def remap_single_value(value, remapping):
+        """
+        Remaps a single value based on remapping dict.
+
+        :param value: (string) string value to be remapped.
+        :param remapping: (dict of strings) remapping from orig_values to new values.
+        :return: (string) remapped string based on remapping. blank string if not in remapping.
+        """
+        return remapping[value] if value in remapping else ''
+
+    """
+    Output Functions
+    """
+    def __str__(self):
+        """
+        Create human-readable string from Person's data
+        :return: (string) Person object in human-friendly string.
+        """
+        output_dict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'year': self.year,
+            'major': self.major,
+            'academic_interests': self.academic_interests,
+            'post_grad_goal': self.post_grad_goal,
+            'software_experience': self.software_experience,
+            'hobbies': self.hobbies
+        }
+        return str(output_dict)
+
+    # TODO
+    def generate_krf(self):
         pass
--- scraping/sheet_scraping.py
+++ scraping/sheet_scraping.py
@@ -8,14 +8,11 @@ from Person import Person
 
 def pull_from_google(url):
     """
-    Pulls raw paper data from Google Sheets
+    Pulls raw people data from Google Sheets.
 
-    Args:
-        url (string): url of a Google sheet to pull data from
-
-    Outputs:
-        (list of strings): header row from sheet
-        (list of lists): individual rows containing paper data
+    :param url: (string): url of a Google sheet to pull data from
+    :return: (list of strings): header row from sheet
+    :return: (list of lists): individual rows containing paper data
     """
     # setup connection
     scope = ['https://spreadsheets.google.com/feeds']
@@ -32,12 +29,59 @@ def pull_from_google(url):
     return header, responses
 
 
+def remap_list_values(orig_values, remapping):
+    """
+    Remaps values in a list to new values based on remapping dict.
+
+    :param orig_values: (list of strings) list of values to be remapped.
+    :param remapping: (dict of strings) remapping from orig_values to new values.
+    :return: (list of strings) list of remapped values based on remappings.
+    """
+    return [remapping[x] if x in remapping else x for x in orig_values]
+
+
+def create_person(field_lookup, data):
+    """
+    Creates a Person object given data for a person and a dict containing mappings from field to index in list for data.
+
+    :param field_lookup: (dict) dict mapping fields needed for person data to indices.
+    :param data: (list) list of data to use for creating person.
+    :return: (Person) new Person object with given data.
+    """
+    return Person(data[field_lookup['first_name']],
+                  data[field_lookup['last_name']],
+                  data[field_lookup['year']],
+                  data[field_lookup['major']],
+                  data[field_lookup['academic_interests']],
+                  data[field_lookup['post_grad_goal']],
+                  data[field_lookup['software_experience']],
+                  data[field_lookup['hobbies']])
+
+
 def main():
     sheet_url = os.environ['SHEET_URL']
     header, rows = pull_from_google(sheet_url)
 
-    print(header)
-    print(rows)
+    # remap headers
+    header_remapping = {
+        'Timestamp': 'timestamp',
+        'First Name': 'first_name',
+        'Last Name': 'last_name',
+        'What is your year in school?': 'year',
+        'What is your major?': 'major',
+        'What are your academic interests?': 'academic_interests',
+        'Currently, what is your post-graduation goal?': 'post_grad_goal',
+        'What software development experience do you have?': 'software_experience',
+        'What are some of your hobbies?': 'hobbies'
+    }
+    remapped_header = remap_list_values(header, header_remapping)
+    header_lookup = {remapped_header[i]: i for i in range(0, len(remapped_header))}
+
+    # create Person objects for each person with their responses
+    people = [create_person(header_lookup, person_data) for person_data in rows]
+
+    for person in people:
+        print(str(person))
 
 
 if __name__ == '__main__':
