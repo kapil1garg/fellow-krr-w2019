5f718c060bcd8db053ff5e7d45069f24daf9325d
Kapil Garg <kapil1garg@gmail.com>
2019-03-04 18:35:57 -0600

    setting up initial scrape from google

--- scraping/sheet_scraping.py
+++ scraping/sheet_scraping.py
@@ -1,10 +1,43 @@
-class Person(object):
-    def __init__(self):
-        pass
+import os
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from operator import itemgetter
+
+from Person import Person
+
+
+def pull_from_google(url):
+    """
+    Pulls raw paper data from Google Sheets
+
+    Args:
+        url (string): url of a Google sheet to pull data from
+
+    Outputs:
+        (list of strings): header row from sheet
+        (list of lists): individual rows containing paper data
+    """
+    # setup connection
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('credential.json', scope)
+    gc = gspread.authorize(credentials)
+
+    # pull papers from Delta Lab paper spreadsheet
+    paper_worksheet = gc.open_by_url(url)
+
+    # split into header and paper data
+    header = paper_worksheet.get_worksheet(0).get_all_values()[0]
+    responses = paper_worksheet.get_worksheet(0).get_all_values()[1:]
+
+    return header, responses
 
 
 def main():
-    pass
+    sheet_url = os.environ['SHEET_URL']
+    header, rows = pull_from_google(sheet_url)
+
+    print(header)
+    print(rows)
 
 
 if __name__ == '__main__':
